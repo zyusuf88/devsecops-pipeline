3ff280d2973cfc9355e6321747647f9870bba3fe
Zeynab Y <97973445+zyusuf88@users.noreply.github.com>
2024-10-15 01:07:36 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -266,20 +266,6 @@ resource "aws_ecs_service" "threat_app_service" {
 # IAM Role and Policy Attachment for ECS Task Execution
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
